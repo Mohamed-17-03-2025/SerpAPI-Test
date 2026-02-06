ad68849a57275018562f5c6e0153ed519e4ea5d9
Mohamed-17-03-2025 <totallygeneric0@gmail.com>
2026-02-06 15:31:19 +0530

    Works. Need to check pagination.

--- main.py
+++ main.py
@@ -4,4 +4,8 @@ from dotenv import load_dotenv
 load_dotenv()
 
 api_key = os.getenv('SERPAPI_KEY')
-client = serpapi.client(api_key=api_key)
+client = serpapi.Client(api_key=api_key)
+
+result = client.search(q="wastewater",engine="google",as_sitesearch="www.omanobserver.om")
+
+print(result)
