ac814ac1c7146c06b813654647d9344fa606ef73
Mohamed-17-03-2025 <totallygeneric0@gmail.com>
2026-02-06 16:18:22 +0530

    Rudimentary fix for pagination.

--- main.py
+++ main.py
@@ -6,6 +6,15 @@ load_dotenv()
 api_key = os.getenv('SERPAPI_KEY')
 client = serpapi.Client(api_key=api_key)
 
-result = client.search(q="wastewater",engine="google",as_sitesearch="www.omanobserver.om")
+articles = []
+
+for iterand in range (0,20):
+    result = client.search(q="wastewater",engine="google",as_sitesearch="www.omanobserver.om",start=iterand*10)
+    articles.append(result)
 
 print(result)
+print("=============================================================================================================================================")
+print(articles)
+
+with open("oman_observer_articles.txt", "w") as output:
+    output.write(str(articles))
